27c3ec339cee39bb8a11ac838b55a2388c194c62
Erdene-Ochir Mendsaikhan <ts.eric@yahoo.com>
2022-03-09 18:47:40 -0500

    update

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -4,6 +4,7 @@ public class StringPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello, World 2222");
+        System.out.println("Java is fun 1 ");
 
     }
 }
