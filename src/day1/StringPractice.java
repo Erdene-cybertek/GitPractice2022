98ed017a5ea1d9db793c442e5da5b3f8eb7bec52
Erdene-Ochir Mendsaikhan <ts.eric@yahoo.com>
2022-03-09 18:41:22 -0500

    update

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,7 +3,7 @@ package day1;
 public class StringPractice {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World 2222");
 
     }
 }
